93918dab134c82d49c34d11b1e2787778107d924
kaiete <kaiete@aragon-press.com>
2022-11-26 19:55:05 +0000

    Woo hoo all ready for lesson 1

---
+++ script.js
@@ -0,0 +1,13 @@
+if (localStorage.getItem("accepted") == true) {
+    location.href = "https://learn.k.vu/lessons/"
+}
+function letsgo() {
+    let x = document.getElementById("continue")
+    var agreed = x.checked
+    if (agreed != true) {
+        alert("You need to accept the Terms to continue.")
+    } else {
+        localStorage.setItem("accepted","true")
+        location.reload()
+    }
+}
