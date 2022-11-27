40acd2e9db5b75f542bd784c509c6607e9e35c08
kaiete <kaiete@aragon-press.com>
2022-11-27 15:46:47 +0000

    worked last time

--- lessons/spanish/script.js
+++
@@ -1,8 +0,0 @@
-function check(x) {
-    let y = document.getElementById(x).value
-    if (y.toLowerCase == "hola") {
-        alert("Correct!")
-    } else {
-        alert("Not quite, but mistakes help you learn!")
-    }
-}
